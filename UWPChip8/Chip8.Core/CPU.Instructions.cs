07794a5cbbe6e680585c24c9ecc560376f811af2
Jody Andrews <jodyandrews@me.com>
2016-08-13 19:09:39 +0100

    Generalised some of the instructions so that the right hand operand is none-register specific. Added additional credits and comments.

--- UWPChip8/Chip8.Core/CPU.Instructions.cs
+++ UWPChip8/Chip8.Core/CPU.Instructions.cs
@@ -10,7 +10,7 @@ namespace Chip8.Core
     {
         public void ExecuteOpCode()
         {
-            switch (_opcode.InstructionType) // Identify upper 4 bits
+            switch (_opcode.InstructionType)
             {
                 case 0x0000:
                     switch (_opcode.Value)
@@ -28,147 +28,154 @@ namespace Chip8.Core
                             break;
                     }
                     break;
-                case 0x1000:
+                case 0x1000: //JP addr
                     JP(_opcode.NNN);
                     break;
-                case 0x2000:
-                    Call(_opcode.Value);
+                case 0x2000: // CALL addr
+                    Call(_opcode.NNN);
                     break;
-                case 0x3000:
+                case 0x3000: // SE Vx, byte
                     SE(_opcode.X, (byte)(_opcode.NN));
                     break;
-                case 0x4000:
+                case 0x4000: // SNE Vx, byte
                     SNE(_opcode.X, _opcode.NN);
                     break;
-                case 0x5000:
+                case 0x5000: // SE Vx, Vy
                     SE(_opcode.X, _v[_opcode.Y]);
                     break;
-                case 0x6000:
-                    _v[_opcode.X] = _opcode.NN;
+                case 0x6000: // LD Vx, byte
+                    LD(_opcode.X, _opcode.NN);
                     break;
-                case 0x7000:
-                    _v[_opcode.X] = (byte)(_v[_opcode.X] + _opcode.NN);
+                case 0x7000: // ADD Vx, byte
+                    ADD(_opcode.X, _opcode.NN);
                     break;
                 case 0x8000:
                     switch (_opcode.N)
                     {
-                        case 0x0000: // 0x8xy0
-                                     // Load value at vy into vx;
+                        case 0x0000: // LD Vx, Vy
                             LD(_opcode.X, _v[_opcode.Y]);
                             break;
-                        case 0x0001: // 0x8xy1
-                            OR(_opcode.X, _opcode.Y);
+                        case 0x0001: // OR Vx, Vy
+                            OR(_opcode.X, _v[_opcode.Y]);
                             break;
-                        case 0x0002:
-                            AND(_opcode.X, _opcode.Y);
+                        case 0x0002: // AND Vx, Vy
+                            AND(_opcode.X, _v[_opcode.Y]);
                             break;
-                        case 0x0003:
-                            XOR(_opcode.X, _opcode.Y);
+                        case 0x0003: // XOR Vx, Vy
+                            XOR(_opcode.X, _v[_opcode.Y]);
                             break;
-                        case 0x0004:
-                            ADD(_opcode.X, _opcode.Y);
+                        case 0x0004: // ADD Vx, Vy
+                            ADD(_opcode.X, _v[_opcode.Y]);
                             break;
-                        case 0x0005:
-                            SUB(_opcode.X, _opcode.Y);
+                        case 0x0005: // SUB Vx, Vy
+                            SUB(_opcode.X, _v[_opcode.Y]);
                             break;
-                        case 0x0006:
+                        case 0x0006: // SHR Vx, 1
                             SHR(_opcode.X);
                             break;
-                        case 0x0007:
-                            SUBN(_opcode.X, _opcode.Y);
+                        case 0x0007: // SUBN Vx, Vy
+                            SUBN(_opcode.X, _v[_opcode.Y]);
                             break;
-                        case 0x000E:
+                        case 0x000E: // SHL Vx, 1
                             SHL(_opcode.X);
                             break;
                     }
                     break;
-                case 0x9000:
+                case 0x9000: // SNE Vx, Vy
                     SNE(_opcode.X, _v[_opcode.Y]);
                     break;
-                case 0xA000:
+                case 0xA000: // LD I, addr
                     LD_I(_opcode.NNN);
                     break;
-                case 0xB000:
-                    Jump_V0((byte)(_opcode.NNN));
+                case 0xB000: // JP V0, addr
+                    JP((ushort)(_opcode.NNN + _v[0]));
                     break;
-                case 0xC000:
+                case 0xC000: // RND Vx, byte
                     RND(_opcode.X, _opcode.NN);
                     break;
-                case 0xD000:
+                case 0xD000: // DRW Vx, Vy, nibble
                     DRW(_opcode.X, _opcode.Y, _opcode.N);
                     break;
-                case 0xE000:
+                case 0xE000: // Input handling
                     switch (_opcode.NN)
                     {
-                        case 0x009E:
-                            if (keys[_v[_opcode.X]] == 1)
-                                _pc += 2;
-
+                        case 0x009E: // SKP Vx
+                            SKP(_v[_opcode.X]);
                             break;
-
-                        case 0x00A1:
-                            if (this.keys[_v[_opcode.X]] != 1)
-                            {
-                                this._pc += 2;
-                            }
+                        case 0x00A1: // SKNP Vx
+                            SKNP(_v[_opcode.X]);
                             break;
                     }
                     break;
                 case 0xF000:
                     switch (_opcode.NN)
                     {
-                        case 0x0007: // Set Vx = delay timer
+                        case 0x0007: // LD Vx, DT
                             LD(_opcode.X, (byte)DelayTimer.Get());
                             break;
-                        case 0x000A:
+                        case 0x000A: // LD Vx, K. TODO : Rough handle for checking key input
                             _halted = true;
 
                             for (var i = 0; i < 16; ++i)
                             {
                                 if (keys[i] != 0)
                                 {
-
                                     _v[_opcode.X] = (byte)i;
 
                                     _halted = false;
                                     continue;
                                 }
                             }
-
                             break;
-                        case 0x0015:
+                        case 0x0015: // LD DT, Vx
                             DelayTimer.Set(_v[_opcode.X]);
                             break;
-                        case 0x0018:
+                        case 0x0018: // LD ST, Vx,
                             SoundTimer.Set(_v[_opcode.X]);
                             break;
-                        case 0x001E:
+                        case 0x001E: // ADD I, Vx
                             I.Set(I.GetValue() + _v[_opcode.X]);
                             break;
-                        case 0x0029:
+                        case 0x0029: // LD F, Vx
                             I.Set(_v[_opcode.X] * 5);
                             break;
-                        case 0x0033:
-                            LD_B(_opcode.X);
+                        case 0x0033: // LD B, Vx
+                            BCD(_opcode.X);
                             break;
-                        case 0x0055:
-                            //LD_I_VX((byte)((opCode & 0x0F00) >> 8));
-
+                        case 0x0055: // LD [I], Vx
                             for (byte i = 0; i <= _opcode.X; i++)
                             {
                                 _memory[I.GetValue() + i] = _v[i];
                             }
                             break;
-                        case 0x0065:
+                        case 0x0065: // LD Vx, [I]
                             for (byte i = 0; i <= _opcode.X; i++)
                             {
                                 _v[i] = _memory[I.GetValue() + i];
                             }
                             break;
                     }
-
-                    break;
+                break;
             }
+        }
+
+        /// <summary>
+        /// ADD Vx, kk
+        /// 
+        /// Adds Vx to kk and stores result at Vx
+        /// 
+        /// Sets carry register if necessary
+        /// 
+        /// Used By : 0x8xy4
+        /// </summary>
+        /// <param name="ax">ax</param>
+        /// <param name="kk">kk</param>
+        void ADD(byte ax, byte kk)
+        {
+            // TODO : Check this. This currently works because the operator + on two 
+            // bytes is implicitly an integer and therefore can exceed 255. Dirty.
+            _v[0xF] = (byte)((_v[ax] + kk > 255) ? 1 : 0);
+            _v[ax] += kk;
         }
 
         /// <summary>
@@ -176,21 +183,21 @@ namespace Chip8.Core
         /// 
         /// Jumps to address kkk
         /// </summary>
-        /// <param name="kkk">kkk.</param>
+        /// <param name="kkk">kkk</param>
         void JP(ushort kkk)
         {
             _pc = kkk;
         }
 
         /// <summary>
-        /// Calls subroutine at the specified address  (opcode & 0x0FFF).
+        /// Calls subroutine at the specified address and stores previous address on stack, advancing stack pointer.
         /// </summary>
-        /// <param name="opcode">Opcode.</param>
-        void Call(int opcode)
+        /// <param name="addr">addr</param>
+        void Call(ushort addr)
         {
             _stack[_sp] = _pc;
+            _pc = addr;
             _sp++;
-            _pc = _opcode.NNN;
         }
 
         /// <summary>
@@ -198,8 +205,8 @@ namespace Chip8.Core
         /// 
         /// Skip next instruction if Vx is equal to kk
         /// </summary>
-        /// <param name="ax">Ax.</param>
-        /// <param name="ay">Ay.</param>
+        /// <param name="ax">ax</param>
+        /// <param name="kk">kk</param>
         void SE(byte ax, byte kk)
         {
             if (_v[ax] == kk)
@@ -209,38 +216,18 @@ namespace Chip8.Core
         }
 
         /// <summary>
-        /// ADD Vx, Vy
+        /// SUB Vx, kk
         /// 
-        /// Adds Vx to Vy and stores result at Vx
-        /// 
-        /// Sets carry register if necessary
-        /// 
-        /// Used By : 0x8xy4
-        /// </summary>
-        /// <param name="ax">ax.</param>
-        /// <param name="ay">ay.</param>
-        void ADD(byte ax, byte ay)
-        {
-            // TODO : Check this
-            _v[0xF] = (byte)((_v[ax] + _v[ay] > 255) ? 1 : 0);
-            _v[ax] += _v[ay];
-        }
-
-        /// <summary>
-        /// SUB Vx, Vy
-        /// 
-        /// Subtracts Vy from Vx and stores value at Vx
+        /// Subtracts kk from Vx and stores value at Vx
         /// 
         /// Sets Vf if borrow not performed
-        /// 
-        /// Used By : 0x8xy5
         /// </summary>
-        /// <param name="ax">ax.</param>
-        /// <param name="ay">ay.</param>
-        void SUB(byte ax, byte ay)
+        /// <param name="ax">ax</param>
+        /// <param name="kk">kk</param>
+        void SUB(byte ax, byte kk)
         {
-            _v[0x0f] = (byte)((_v[ax] >= _v[ay]) ? 1 : 0);
-            _v[ax] -= _v[ay];
+            _v[0x0f] = (byte)((_v[ax] >= kk) ? 1 : 0);
+            _v[ax] -= kk;
         }
 
         /// <summary>
@@ -250,7 +237,7 @@ namespace Chip8.Core
         /// 
         /// Sets Vf to LSB
         /// </summary>
-        /// <param name="vx">Vx.</param>
+        /// <param name="vx">ax</param>
         void SHR(byte ax)
         {
             _v[0xf] = (byte)(_v[ax] & 0x1);
@@ -258,28 +245,28 @@ namespace Chip8.Core
         }
 
         /// <summary>
-        /// SUBN Vx, Vy : 0x8XY7
+        /// SUBN Vx, kk
         /// 
-        /// Subtracts Vx from Vy and stores value in Vx
+        /// Subtracts Vx from kk and stores value in Vx
         /// 
         /// Sets Vf if borrow not performed.
         /// </summary>
-        /// <param name="ax">Ax.</param>
-        /// <param name="ay">Ay.</param>
-        void SUBN(byte ax, byte ay)
+        /// <param name="ax">ax</param>
+        /// <param name="kk">kk</param>
+        void SUBN(byte ax, byte kk)
         {
-            _v[0xf] = (byte)((_v[ax] <= _v[ay]) ? 1 : 0);
-            _v[ax] = (byte)(_v[ay] - _v[ax]);
+            _v[0xf] = (byte)((_v[ax] <= kk) ? 1 : 0);
+            _v[ax] = (byte)(kk - _v[ax]);
         }
 
         /// <summary>
-        /// SHL Vx : 0x8XYE
+        /// SHL Vx
         /// 
         /// Logical Shift Left (1)
         /// 
         /// Sets Vf to MSB
         /// </summary>
-        /// <param name="ax">Ax.</param>
+        /// <param name="ax">ax</param>
         void SHL(byte ax)
         {
             _v[0xf] = (byte)(_v[ax] >> 7);
@@ -287,12 +274,12 @@ namespace Chip8.Core
         }
 
         /// <summary>
-        /// SNE Vx, kk : 0x9XY0
+        /// SNE Vx, kk
         /// 
         /// Skips next instruction if Vx does not equal kk
         /// </summary>
-        /// <param name="ax">Ax.</param>
-        /// <param name="ay">Ay.</param>
+        /// <param name="ax">ax.</param>
+        /// <param name="kk">kk.</param>
         void SNE(byte ax, byte kk)
         {
             if (_v[ax] != kk)
@@ -300,39 +287,39 @@ namespace Chip8.Core
         }
 
         /// <summary>
-        /// OR Vx, Vy : 0x8XY1
+        /// OR Vx, kk
         /// 
-        /// Bitwise OR on Vx and Vy and store result in Vx
+        /// Bitwise OR on Vx and kk and store result in Vx
+        /// </summary>
+        /// <param name="ax">ax</param>
+        /// <param name="kk">kk</param>
+        void OR(byte ax, byte kk)
+        {
+            _v[ax] = (byte)(_v[ax] | kk);
+        }
+
+        /// <summary>
+        /// AND Vx, kk
+        /// 
+        /// Bitwise AND on Vx and kk and store result in Vx
+        /// </summary>
+        /// <param name="ax">ax</param>
+        /// <param name="kk">kk</param>
+        void AND(byte ax, byte kk)
+        {
+            _v[ax] = (byte)(_v[ax] & kk);
+        }
+
+        /// <summary>
+        /// XOR Vx, kk
+        /// 
+        /// Bitwise exclusive OR on Vx and kk and store result in Vx
         /// </summary>
         /// <param name="ax">ax.</param>
-        /// <param name="ay">ay.</param>
-        void OR(byte ax, byte ay)
+        /// <param name="kk">kk</param>
+        void XOR(byte ax, byte kk)
         {
-            _v[ax] = (byte)(_v[ax] | _v[ay]);
-        }
-
-        /// <summary>
-        /// AND Vx, Vy : 0x8xy2
-        /// 
-        /// Bitwise AND on Vx and By and store result in Vx
-        /// </summary>
-        /// <param name="ax">Vx.</param>
-        /// <param name="ay">Vy.</param>
-        void AND(byte ax, byte ay)
-        {
-            _v[ax] = (byte)(_v[ax] & _v[ay]);
-        }
-
-        /// <summary>
-        /// XOR Vx, Vy
-        /// 
-        /// Bitwise exclusive OR on Vx and Vy and store result in Vx
-        /// </summary>
-        /// <param name="ax">Vx.</param>
-        /// <param name="ay">Vy.</param>
-        void XOR(byte ax, byte ay)
-        {
-            _v[ax] = (byte)(0xFF & (_v[ax] ^ _v[ay]));
+            _v[ax] = (byte)(0xFF & (_v[ax] ^ kk));
         }
 
         /// <summary>
@@ -346,29 +333,38 @@ namespace Chip8.Core
             I.Set(value);
         }
 
-        void Jump_V0(int addr)
-        {
-            _pc = addr + _v[0];
-        }
-
         /// <summary>
         /// RND Vx, kk : CXKK
         /// 
         /// Generates a random byte bitwise AND with kk and result stored in Vx
         /// </summary>
-        /// <param name="ax">Ax.</param>
-        /// <param name="kk">Kk.</param>
+        /// <param name="ax">ax</param>
+        /// <param name="kk">kk</param>
         void RND(byte ax, byte kk)
         {
             byte rndVal = (byte)_rnd.Next(0, 255 + 1);
             _v[ax] = (byte)(rndVal & kk);
         }
 
-        void LD(byte vx, byte kk)
+        /// <summary>
+        /// Loads value kk into address ax
+        /// </summary>
+        /// <param name="ax"></param>
+        /// <param name="kk"></param>
+        void LD(byte ax, byte kk)
         {
-            _v[vx] = kk;
+            _v[ax] = kk;
         }
 
+        /// <summary>
+        /// 'Draw's a pixel to the display buffer
+        /// 
+        /// This is more or less a direct implementation of Alexander Dickson's JS
+        /// Chip8 Emulator
+        /// </summary>
+        /// <param name="ax"></param>
+        /// <param name="ay"></param>
+        /// <param name="nibble"></param>
         void DRW(byte ax, byte ay, byte nibble)
         {
             _v[0xf] = 0;
@@ -394,6 +390,12 @@ namespace Chip8.Core
             DrawFlag = true;
         }
 
+        /// <summary>
+        /// Sets a pixel from the DRW call
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
         bool SetPixel(byte x, byte y)
         {
             var width = 64;
@@ -424,18 +426,33 @@ namespace Chip8.Core
             return !Convert.ToBoolean(_displayBuffer[location]);
         }
 
+        /// <summary>
+        /// Skips next instruction is key IS pressed
+        /// </summary>
+        /// <param name="ax"></param>
         void SKP(byte ax)
         {
-            throw new NotImplementedException();
+            if (keys[ax] == 1)
+                _pc += 2;
         }
 
+        /// <summary>
+        /// Skips next instruction if key is NOT pressed
+        /// </summary>
+        /// <param name="ax"></param>
         void SKNP(byte ax)
         {
-            //_pc++;
-            throw new NotImplementedException();
+            if (this.keys[ax] != 1)
+                this._pc += 2;
         }
 
-        void LD_B(byte ax)
+        /// <summary>
+        /// Stores BCD representation of Vx in memory locations I, I+1, I+2
+        /// 
+        /// This is a direct implementation of Alexander Dicksons BCD function
+        /// </summary>
+        /// <param name="ax"></param>
+        void BCD(byte ax)
         {
             var number = _v[ax];
 
